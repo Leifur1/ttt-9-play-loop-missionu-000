065d4f6fc372042e9ae81a6ff728a43f7555b88e
Leifur Sigurdsson <leifurerik@gmail.com>
2018-09-27 23:50:02 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -36,9 +36,10 @@ def turn(board)
 end
 
 # Define your play method below
-def play(turn)
-  count = 0
+def play(board)
+  count = 0 
   while count != 9
+    turn(board)
     count += 1
   end
 end
